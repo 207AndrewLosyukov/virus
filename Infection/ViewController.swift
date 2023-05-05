c72118da9bfde97622458e8b54baf3a7041dfe4b
Andrew Losyukov <avlosyukov@edu.hse.ru>
2023-05-05 16:26:01 +0300

    Initial Commit

---
+++ Infection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Infection
+//
+//  Created by Андрей Лосюков on 05.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
